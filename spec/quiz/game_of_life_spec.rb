c4653d3d10afe29fe31c6571cf0c8e22aa74e496
ChenLucas <chucobo5219@gmail.com>
2017-04-16 22:03:59 +0800

    design basic test case

---
+++ spec/quiz/game_of_life_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+RSpec.describe GameOfLife do
+  describe '#initialize' do
+  end
+
+  describe '#kill_cell' do
+    let(:board) do
+      [
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 0, 1]
+      ]
+    end
+    let(:expected) do
+      [
+        [0, 1, 1],
+        [0, [1, 0], 1],
+        [0, 0, 1]
+      ]
+    end
+    subject { GameOfLife.new(board).kill_cell([2, 3]) }
+    it { is_expected.to be false }
+  end
+
+  describe '#keep_cell_alive' do
+  end
+
+  describe '#grow_out_cells' do
+  end
+
+  describe '#check_cell' do
+    let(:board) do
+      [
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 0, 1]
+      ]
+    end
+    let(:selector) { [1, 1] }
+    subject { GameOfLife.new(board).send(:check_cell, selector) }
+    it { is_expected.to eq true: 4, false: 4 }
+    context 'when select right buttom' do
+      let(:selector) { [2, 2] }
+      it { is_expected.to eq true: 3, false: 1 }
+    end
+  end
+
+  describe '#gather_around_cell' do
+    let(:board) do
+      [
+        [0, 1, 1],
+        [0, 1, 1],
+        [0, 0, 1]
+      ]
+    end
+    let(:selector) { [1, 1] }
+    subject { GameOfLife.new(board).send(:gather_around_cell, selector) }
+    it { is_expected.to eq [0, 1, 1, 0, 0, 1, 0, 1] }
+    context 'when select right buttom' do
+      let(:selector) { [2, 2] }
+      it { is_expected.to eq [0, 1, 1] }
+    end
+  end
+end
