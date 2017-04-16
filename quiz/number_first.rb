47ad0bb3104da6ff883b697172cdc328f77bfbaf
ChenLucas <chucobo5219@gmail.com>
2017-04-16 19:36:43 +0800

    make it pass the first test case

--- quiz/number_first.rb
+++ quiz/number_first.rb
@@ -15,6 +15,20 @@ class NumberFirst
   # @param [Array] array a list of array
   # @return [Array] An Array in special ordering
   def self.sorting(array)
-
+    cache_block = array.count
+    array.count.times do |index|
+      if array[index] == 0
+        (array.count - 1 - index).times do |change_index|
+          switch_index = index + change_index
+          next if array[switch_index] == 0
+          array[cache_block] = array[index]
+          array[index] =  array[switch_index]
+          array[switch_index] = array[cache_block]
+          break
+        end
+      end
+    end
+    array.delete_at(cache_block)
+    array
   end
 end
--- spec/quiz/number_first_spec.rb
+++ spec/quiz/number_first_spec.rb
@@ -2,5 +2,8 @@ require 'spec_helper'
 
 RSpec.describe NumberFirst do
   describe '#self.sorting' do
+    let(:array_params) { [0, 1, 0, 3, 12] }
+    subject { NumberFirst.sorting(array_params)}
+    it { is_expected.to eq [1, 3, 12, 0, 0] }
   end
 end
