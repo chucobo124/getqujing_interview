acabd6d2dff673748b6780e6040d9b4397f2dc9e
ChenLucas <chucobo5219@gmail.com>
2017-04-16 19:00:10 +0800

    Initialize number first function

---
+++ quiz/number_first.rb
@@ -0,0 +1,20 @@
+# Number first
+#
+# Given an array nums, write a function to move all non-zero numbers to the head
+# of it while maintaining the relative order of the non-zero numbers.
+#
+# For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums
+# should be [1, 3, 12, 0, 0].
+#
+# Note: You must do this in-place without making a copy of the array. Minimize
+# the total number of operations.
+
+class NumberFirst
+  # Srot the array in special ordering
+  #
+  # @param [Array] array a list of array
+  # @return [Array] An Array in special ordering
+  def self.sorting(array)
+
+  end
+end
---
+++ spec/quiz/number_first_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+RSpec.describe NumberFirst do
+  describe '#self.sorting' do
+  end
+end
