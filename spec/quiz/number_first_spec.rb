b1e9b7704d85ce4b1a85dc6d99214dfd4cbe67a2
ChenLucas <chucobo5219@gmail.com>
2017-04-16 19:39:26 +0800

    Add more test case

--- spec/quiz/number_first_spec.rb
+++ spec/quiz/number_first_spec.rb
@@ -3,7 +3,15 @@ require 'spec_helper'
 RSpec.describe NumberFirst do
   describe '#self.sorting' do
     let(:array_params) { [0, 1, 0, 3, 12] }
-    subject { NumberFirst.sorting(array_params)}
+    subject { NumberFirst.sorting(array_params) }
     it { is_expected.to eq [1, 3, 12, 0, 0] }
+    context 'when number has negative number' do
+      let(:array_params) { [0, 0, -1, 1, 0, 3, 12] }
+      it { is_expected.to eq [-1, 1, 3, 12, 0, 0, 0] }
+    end
+    context 'when zero is alreday sorting' do
+      let(:array_params) { [3, 12, 0, 0, 0] }
+      it { is_expected.to eq [3, 12, 0, 0, 0] }
+    end
   end
 end
