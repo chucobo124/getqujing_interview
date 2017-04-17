5a73556f16d1afd597b6e847c326c248177d3bcf
ChenLucas <chucobo5219@gmail.com>
2017-04-17 17:01:50 +0800

    Implement function and add more test case

--- quiz/simple_root_square.rb
+++ quiz/simple_root_square.rb
@@ -8,6 +8,19 @@ class SimpleRootSquare
   # @param [integer] Number The number for root square
   # @return [integer] root square result
   def process(number)
-    
+    return 0 if number < 0
+    result = 0
+    maxium_bit = 1 << 14
+    result >> 2 while result > number
+    while maxium_bit != 0
+      if number >= result + maxium_bit
+        number -= result + maxium_bit
+        result = (result >> 1) + maxium_bit
+      else
+        result = result >> 1
+      end
+      maxium_bit = maxium_bit >> 2
+    end
+    result
   end
 end
